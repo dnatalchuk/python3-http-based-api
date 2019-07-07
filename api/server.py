fe1f7e3e6678e5856d0e8e057f741902d0c6bd08
Denys Natalchuk <denys.natalchuk@kohls.com>
2019-07-07 18:02:32 +0200

    update implementation

--- api/server.py
+++ api/server.py
@@ -7,15 +7,46 @@ from sqlite3 import Error
 from sqlalchemy import create_engine
 
 app = flask.Flask(__name__)
-db_connect = create_engine('sqlite:///users.db')
 
 
-@app.route('/', methods=['GET'])
-def api_all():
-    conn = db_connect.connect()
-    query = conn.execute('SELECT * FROM Users;')
-    result = query.cursor.fetchall()
-    return jsonify(result)
+# @app.route('/hello', methods=['GET'])
+# def api_get():
+#     conn = sqlite3.connect('users.db')
+#     c = conn.cursor()
+#     query = c.execute('SELECT * FROM Users;')
+#     result = query.fetchall()
+#     return jsonify(result)
+
+
+@app.route('/hello/<username>', methods=['GET', 'POST'])
+def api_put(username):
+    if request.method == 'GET':
+        user = "{}".format(username)
+        conn = sqlite3.connect('users.db')
+        cur = conn.cursor()
+        query = cur.execute("SELECT DATE_OF_BIRTH FROM USERS WHERE USERNAME=?", (user,))
+        dateOfBirth = query.fetchall()
+        return jsonify("message: Hello, {}! Your birthday at {}".format(username, dateOfBirth))
+
+
+    if request.method == 'POST':
+        req_data = request.get_json()
+        dateOfBirth = req_data['dateOfBirth']
+        with sqlite3.connect('users.db') as conn:
+            user = "{}".format(username)
+            conn = sqlite3.connect('users.db')
+            cur = conn.cursor()
+            query = cur.execute("SELECT DATE_OF_BIRTH FROM USERS WHERE USERNAME=?", (user,))
+            result = query.fetchall()
+            if len(result)==0:
+                conn.execute('INSERT INTO USERS (USERNAME,DATE_OF_BIRTH) VALUES (?,?)', (username, dateOfBirth))
+                conn.commit()
+                return jsonify("Record for {} and date of birth {} successfully ADDED".format(username, dateOfBirth))
+            else:
+                conn.execute('UPDATE USERS SET DATE_OF_BIRTH=? WHERE USERNAME=?', (dateOfBirth, username))
+                conn.commit()
+                return jsonify("Record for {} has been UPDATED with date of birth {}".format(username, dateOfBirth))
+
 
 if __name__ == '__main__':
     app.run(debug=True)
