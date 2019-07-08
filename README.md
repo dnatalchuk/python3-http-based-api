e2e7ab4c864e75e3e660a5868cc1cb7031303b2c
Denys Natalchuk <denys.natalchuk@kohls.com>
2019-07-08 08:40:25 +0200

    finish implementation and docs

--- README.md
+++ README.md
@@ -1,1 +1,75 @@
-![alt text](https://www.komisjamasters.pl/wp-content/uploads/2018/10/Challenge.arrow_-678x381.jpg)
+# Application that exposes HTTP-based API (implemented with Python Flask)
+
+> Handle HTTP requests to update and fetch data from the database.
+
+## Table of contents and structure
+
+```
+├── README.md
+├── api
+│   ├── server.py
+│   └── users.db
+├── database
+│   └── db.py
+├── readme_images
+├── requirements.txt
+└── users.db
+└── venv
+
+```
+
+## Scope:
+* `api/server.py`:
+```
+    1. creates API with the Flask framework used;
+    2. GET and POST (only application/json content type)
+       methods are supported;
+    3. API makes calls to Sqlite3 database for updating
+       and fetching data;
+    4. endpoint exposed /hello/<username> with dynamic
+       lookup value for username in request data;
+    5. Implemented logic for updating values in DB if the username exists
+       and create a new one - if missing;
+    6. Username should be a unique value;
+    7. The logic for birthday delta calculation is
+       applied and respective messages displayed;
+```
+* `api/user.db` - Sqlite3 database with the records;
+* `database/db.py`:
+```
+    1. creates a Sqlite3 database, with the specified;
+    2. creates a table in the database;
+```
+
+## Usage:
+* Start the application with next commands:
+
+```
+    $ cd challenge/
+    $ source venv/bin/activate
+    $ cd api/
+    $ ./server.py
+```
+
+* Make a POST request with sending body message as a JSON (use curl or Postman):
+
+```
+* endpoint: http://127.0.0.1:5000/hello/<username>
+* request: {"dateOfBirth":"1998-01-01"}
+```
+![alt text](readme_images/1.png)
+```
+* if the same username will be used - the record will be updated:
+```
+![alt text](readme_images/2.png)
+
+* Get request for a username and respective messages displayed for a birthday:
+```
+If birthday is today:
+```
+![alt text](readme_images/3.png)
+
+```
+Any other day:
+```
+![alt text](readme_images/4.png)
--- api/server.py
+++ api/server.py
@@ -1,33 +1,64 @@
 #!/usr/bin/env python3
 
+"""
+  This module has the following purposes:
+    1. creates API with the Flask framework used;
+    2. GET and POST (only application/json content type)
+       methods are supported;
+    3. API makes calls to Sqlite3 database for updating
+       and fetching data;
+    4. endpoint exposed /hello/<username> with dynamic
+       lookup value for username in request data;
+    5. Implemented logic for updating values in DB if the username exists
+       and create a new one - if missing;
+    6. Username should be a unique value;
+    7. The logic for birthday delta calculation is
+       applied and respective messages displayed;
+
+Example:
+    $ cd challenge/
+    $ source venv/bin/activate
+    $ cd api/
+    $ ./server.py
+"""
+
 import flask
 from flask import request, jsonify
 import sqlite3
-from sqlite3 import Error
-from sqlalchemy import create_engine
+import datetime
+from datetime import date
+
 
 app = flask.Flask(__name__)
 
 
-# @app.route('/hello', methods=['GET'])
-# def api_get():
-#     conn = sqlite3.connect('users.db')
-#     c = conn.cursor()
-#     query = c.execute('SELECT * FROM Users;')
-#     result = query.fetchall()
-#     return jsonify(result)
-
-
 @app.route('/hello/<username>', methods=['GET', 'POST'])
-def api_put(username):
+def api(username):
     if request.method == 'GET':
         user = "{}".format(username)
         conn = sqlite3.connect('users.db')
         cur = conn.cursor()
-        query = cur.execute("SELECT DATE_OF_BIRTH FROM USERS WHERE USERNAME=?", (user,))
-        dateOfBirth = query.fetchall()
-        return jsonify("message: Hello, {}! Your birthday at {}".format(username, dateOfBirth))
+        query = cur.execute("SELECT DATE_OF_BIRTH FROM USERS WHERE\
+        USERNAME=?", (user,))
+        dateOfBirth = cur.fetchone()
+        date_split = dateOfBirth[0].split('-')
+        y, m, d = date_split
+        today = datetime.date.today()
+        year = date.today().year
+        birthday = date(int(year), int(m), int(d))
+        if today > birthday:
+            year += 1
+            birthday = date(int(year), int(m), int(d))
+            delta = abs((today - birthday).days)
+        elif today == birthday:
+            return jsonify("message: Hello, {}! Happy birthday"
+                           .format(username))
+        else:
+            birthday = date(int(year), int(m), int(d))
+            delta = (birthday - today).days
 
+        return jsonify("message: Hello, {}! Your birthday in {} day(s)"
+                       .format(username, delta))
 
     if request.method == 'POST':
         req_data = request.get_json()
@@ -36,16 +67,21 @@ def api_put(username):
             user = "{}".format(username)
             conn = sqlite3.connect('users.db')
             cur = conn.cursor()
-            query = cur.execute("SELECT DATE_OF_BIRTH FROM USERS WHERE USERNAME=?", (user,))
+            query = cur.execute("SELECT DATE_OF_BIRTH FROM USERS WHERE\
+             USERNAME=?", (user,))
             result = query.fetchall()
-            if len(result)==0:
-                conn.execute('INSERT INTO USERS (USERNAME,DATE_OF_BIRTH) VALUES (?,?)', (username, dateOfBirth))
+            if len(result) == 0:
+                conn.execute('INSERT INTO USERS (USERNAME,DATE_OF_BIRTH)\
+                VALUES (?,?)', (username, dateOfBirth))
                 conn.commit()
-                return jsonify("Record for {} and date of birth {} successfully ADDED".format(username, dateOfBirth))
+                return jsonify("Record for {} and date of birth {} successfully\
+                ADDED".format(username, dateOfBirth))
             else:
-                conn.execute('UPDATE USERS SET DATE_OF_BIRTH=? WHERE USERNAME=?', (dateOfBirth, username))
+                conn.execute('UPDATE USERS SET DATE_OF_BIRTH=? WHERE \
+                USERNAME=?', (dateOfBirth, username))
                 conn.commit()
-                return jsonify("Record for {} has been UPDATED with date of birth {}".format(username, dateOfBirth))
+                return jsonify("Record for {} has been UPDATED with date of \
+                            birth {}".format(username, dateOfBirth))
 
 
 if __name__ == '__main__':
